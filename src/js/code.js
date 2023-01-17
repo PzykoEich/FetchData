70851da9af3ba0959987d2fcfcd437813e03cbed
Martín Ocampo <Pzykoh@gmail.com>
2023-01-17 19:47:00 +0000

    All

--- src/js/code.js
+++ src/js/code.js
@@ -1,3 +1,4 @@
+//
 const fetch = require('node-fetch');
 
 const url = 'https://covid-193.p.rapidapi.com/statistics?country=';
@@ -10,7 +11,35 @@ const options = {
   }
 };
 
-fetch(`${url}mexico`, options)
+
+
+// inputs
+const selectCountry = document.querySelector('.selectCountry');
+const country = document.querySelector(".country");
+const cases = document.querySelector('.cases');
+const date = document.querySelector('.date');
+
+//botones
+const btnSelectCountry = document.querySelector('.btn-select-country');
+const btnNew = document.querySelector('.sub-form');
+
+btnSelectCountry.addEventListener('click', consulta);
+btnNew.addEventListener('click', nuevo);
+country.set
+
+
+
+
+function consulta(){
+    let countrys = selectCountry.value;
+    url = url + countrys
+    fetch(url, options)
 	.then(res => res.json())
 	.then(json => console.log(json))
 	.catch(err => console.error('error:' + err));
+    console.log(countrys.trim());
+}
+
+function nuevo(){
+    console.log('nuevo');
+}
